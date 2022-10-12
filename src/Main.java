f0aade2a287974460ff60d3f0b8ff276edfc4e0f
bwskyA <adamjakubowski1511@icloud.com>
2022-10-12 09:40:17 +0200

    content

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        Person person = new Person("Adam", "00301412345", "Warszawa", 1.75);
+        Person person2 = new Person("Jarek", "97101412345", "Lodz", 1.90);
+        Person person3 = new Person("Ania", "03312512345", "Warszawa", 1.50);
+        Person person4 = new Person("Kasia", "87113012345", "Lodz", 1.86);
+
+        //Zostawione celowo, zeby sie latwiej sprawdzalo
+        //Person.showPersons();
+        //System.out.println();
+
+        System.out.println("Exercise 1");
+        System.out.println("The oldest person is " + Person.theOldestPerson());
+        System.out.println();
+
+        System.out.println("Exercise 2");
+        System.out.println("There are " + Person.peopleFromCity("Lodz") + " person/s in your chosen city");
+        System.out.println();
+
+        System.out.println("Exercise 3");
+        System.out.println("The average age in the selected city is: " + Person.averageCityAge("Warszawa"));
+        System.out.println();
+
+        System.out.println("Exercise 4");
+        System.out.println("The oldest person in selected city is: " + Person.theOldestInCity("Warszawa"));
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,157 @@
-package PACKAGE_NAME;public class Person {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Person {
+    private String name, pesel, city;
+    private int age;
+    private double height;
+    private Pattern pattern = Pattern.compile("(?<year>[0-9]{2})(?<month>[02468]1|[13579][012])(?<day>0[1-9]|1[0-9]|2[0-9]|3[01])[0-9]{5}");
+    private static List<Person> personList = new ArrayList<>();
+
+    public Person(String name, String pesel, String city, double height) throws Exception {
+        this.name = name;
+        this.city = city;
+        this.height = height;
+        setPesel(pesel);
+        countAge(pesel);
+
+        personList.add(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) throws Exception {
+        if (checkPesel(pesel)) {
+            this.pesel = pesel;
+        } else {
+            throw new Exception("Invalid Pesel!!");
+        }
+    }
+
+    public static Person theOldestPerson() {
+        int age = 0;
+        Person theOldest = null;
+
+        for (Person p : personList) {
+            if (p.getAge() > age) {
+                age = p.getAge();
+                theOldest = p;
+            }
+        }
+        return theOldest;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void showPersons() {
+        for (Person p : personList) {
+            System.out.println(p);
+        }
+    }
+
+    public boolean checkPesel(String pesel) {
+        Matcher matcher = pattern.matcher(pesel);
+        return matcher.matches();
+    }
+
+    public void countAge(String pesel) {
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        Matcher matcher = pattern.matcher(pesel);
+        matcher.matches();
+        String bornYear = matcher.group("year");
+        String bornXX = "19";
+        String bornXXI = "20";
+
+        if (Integer.parseInt(matcher.group("month")) > 12) {
+            bornXXI = new StringBuilder(bornXXI).append(bornYear).toString();
+            setAge(year - Integer.parseInt(bornXXI));
+        } else {
+            bornXX = new StringBuilder(bornXX).append(bornYear).toString();
+            setAge(year - Integer.parseInt(bornXX));
+        }
+    }
+
+    public static Integer peopleFromCity(String city) {
+        List<Person> peopleFromCity = new ArrayList<>();
+        for (Person p : personList) {
+            if (p.getCity().equals(city)) {
+                peopleFromCity.add(p);
+            }
+        }
+        return peopleFromCity.size();
+    }
+
+    public static double averageCityAge(String city) {
+        List<Person> cityPeople = new ArrayList<>();
+        double sumPeopleAge = 0;
+
+        for (Person p : personList) {
+            if (p.getCity().equals(city)) {
+                cityPeople.add(p);
+            }
+        }
+        for (Person p : cityPeople) {
+            sumPeopleAge += p.getAge();
+        }
+        return sumPeopleAge / cityPeople.size();
+    }
+
+    public static Person theOldestInCity(String city){
+        Person theOldest = null;
+        int age = 0;
+
+        for (Person p: personList){
+            if (p.getCity().equals(city)){
+                if (p.getAge() > age){
+                    age = p.getAge();
+                    theOldest = p;
+                }
+            }
+        }
+        return theOldest;
+    }
+
+    @Override
+    public String toString() {
+        return "Name='" + name + '\'' +
+                ", pesel='" + pesel + '\'' +
+                ", city='" + city + '\'' +
+                ", height='" + height + '\'' +
+                ", age='" + age + "\'";
+    }
 }
